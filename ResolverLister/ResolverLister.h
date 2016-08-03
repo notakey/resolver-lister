92657656d00ee4838af5a8fbf5bede42c5f69908
Janis Kirsteins <janis@montadigital.com>
2016-08-03 10:47:51 +0300

    Initial Commit

---
+++ ResolverLister/ResolverLister.h
@@ -0,0 +1,19 @@
+//
+//  ResolverLister.h
+//  ResolverLister
+//
+//  Created by Janis Kirsteins on 03.08.16.
+//  Copyright © 2016. g. Notakey Latvia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ResolverLister.
+FOUNDATION_EXPORT double ResolverListerVersionNumber;
+
+//! Project version string for ResolverLister.
+FOUNDATION_EXPORT const unsigned char ResolverListerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResolverLister/PublicHeader.h>
+
+
